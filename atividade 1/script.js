6819ebd88a9c0c651dcf0247f872ae063fe519f3
Marcos2k02 <marquinhosmaciel44@gmail.com>
2021-11-05 13:01:25 -0300

    Funcoes

---
+++ atividade 1/script.js
@@ -0,0 +1,23 @@
+// Questão 1
+ 
+var array = () => {
+    var array = ["Acre", "Amapá", "Amazonas"];
+    array.reverse();
+    return array;
+  }
+
+// Questão 2
+
+var strings = () => {
+    var str1 = "pedro ";
+    var str2 = "gabriel";
+    var maiuscula = str1.toUpperCase();
+    var concat = maiuscula.concat(str2);
+    return concat;
+  }
+// Questão 3
+
+var repeticao = () => {
+    var rep = "Repetição - ";
+    return (rep.repeat(17));
+  }
---
+++ atividade 2/script.js
@@ -0,0 +1,13 @@
+// Questão 1
+
+var parametro = (numero, percentual) => {
+    let calculo = (numero/100) * percentual;
+    return calculo;
+  }
+// Questão 2
+
+var arrays = (array1, array2) => {
+    var juncao = array1.concat(array2);
+    var str = juncao.toString();
+    return str;
+  }
